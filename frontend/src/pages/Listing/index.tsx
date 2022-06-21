78cb05a53f323fa44fef4c7cc2589e038c03f84a
john <johnsdj68@gmail.com>
2022-06-21 00:57:37 -0300

    FIRST request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,16 @@
+import axios from "axios";
 import MovieCard from "componente/MovieCard";
 import Pagination from "componente/Pagination";
+import { BASE_URL } from"utils/requests"
 function Listing() {
+
+//FORMA ERRADA
+axios.get(`${BASE_URL}/movies?size=12&page=0`)
+.then(response =>{
+  console.log(response.data);
+})
+
+
   return (
     <>
 
