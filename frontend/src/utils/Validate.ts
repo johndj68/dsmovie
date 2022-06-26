d44dde84f008c170a8f09775bc7087f94016201e
john <johnsdj68@gmail.com>
2022-06-26 13:45:29 -0300

    ATT

--- frontend/src/utils/Validate.ts
+++ frontend/src/utils/Validate.ts
@@ -1,4 +1,4 @@
-// https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+
 export function validateEmail(email: any) {
     return String(email)
       .toLowerCase()
